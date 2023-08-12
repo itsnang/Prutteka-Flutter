13335591774dddf4ed43df84f24c19c9db150a3d
itsnang <lorn.samnang@cellcard.com.kh>
2023-08-12 11:22:17 +0700

    feat: setup prutteka theme

---
+++ lib/shared/themes/color_theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ColorTheme {
+  ColorTheme._();
+  static const Color primaryColor = Color(0xFFFF006E);
+  static const Color primaryLight = Color(0xFFFFE6F1);
+  static const Color primaryDark = Color(0xFFB3004D);
+  static const Color secondary = Color(0xFF3A86FF);
+  static const Color tertiary = Color(0xFFFFBE0B);
+  static const Color tertiaryLight = Color(0xFFFFF9E7);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color backgroundDark = Color(0xFF000000);
+
+  static const Color grey50 = Color(0xFFFAFAFA);
+  static const Color grey100 = Color(0xFFF4F4F5);
+  static const Color grey200 = Color(0xFFE4E4E7);
+  static const Color grey300 = Color(0xFFD4D4D8);
+  static const Color grey400 = Color(0xFFA3A3A3);
+  static const Color grey500 = Color(0xFF737373);
+  static const Color grey600 = Color(0xFF52525B);
+  static const Color grey700 = Color(0xFF3F3F46);
+  static const Color grey800 = Color(0xFF27272A);
+  static const Color grey900 = Color(0xFF27272A);
+}
---
+++ lib/shared/widgets/event_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EventCard extends StatelessWidget {
+  const EventCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
