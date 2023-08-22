c38fb9e6d6c091ec889e982ef5a508082e25692a
Sophearom Sean <sophearom.sean@canadiabank.com.kh>
2023-08-22 09:24:05 +0700

    add event_route dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:prutteka_flutter/app/util/dependency.dart';
 import 'package:prutteka_flutter/initialized_app.dart';
 
 void main() {
+  DependencyCreator.init();
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const InitializedApp());
 }
